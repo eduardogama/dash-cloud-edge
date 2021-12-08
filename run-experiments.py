a195301acc2166f4424208972065ba5bca249609
Eduardo Gama <eduardogama72@gmail.com>
2021-12-08 07:59:47 -0300

    Adding run-experiments.py and env files to run the simulations

---
+++ run-experiments.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+def main():
+	
+	rep 	 = os.getenv('Repetition')
+	stopTime = os.getenv('StopTime') # 
+	users 	 = os.getenv('Users') # 10
+	protocol = os.getenv('AdaptationLogicToUse') # RateBasedAdaptationLogic RateAndBufferBasedAdaptationLogic 
+
+
+	print("Initializing ...")
+
+	for seed in range(0,rep):
+		os.system('./waf --run \'dash-btree-botup --stopTime=%d --users=%d --seed=%d --AdaptationLogicToUse=\"dash::player::%s\"\'' % (stopTime, users, seed, protocol))
+
+	print("Done.")
+
+
+if __name__ == '__main__':
+	main()
