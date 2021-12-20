932ed1dc4022dceda8a280240657eef14a242151
Eduardo Gama <eduardogama72@gmail.com>
2021-12-19 23:42:13 -0300

    Changing Pool to Process multiprocessing.

--- multi-experiments.py
+++ multi-experiments.py
@@ -1,41 +1,52 @@
 import os
 from dotenv import load_dotenv
-from multiprocessing import Process
+from multiprocessing import Pool
 
 
 load_dotenv()
 
 
+sim_params = {
+    'cores': int(os.getenv('cores')),
+    'users': int(os.getenv('Users')),  # 15
+    'rep': int(os.getenv('Repetition')),
+    'stopTime': int(os.getenv('StopTime')),
+    'protocol': os.getenv('AdaptationLogicToUse'),  # RateBasedAdaptationLogic RateAndBufferBasedAdaptationLogic
+}
+
+
 def start_simulation(sim_params, seed):
+    # os.system('./waf --run \'dash-btree-botup --stopTime=%d --Client=%d --seed=%d --AdaptationLogicToUse=\'%s\'\'' %
+    #           (sim_params['stopTime'], sim_params['users'], seed, sim_params['protocol']))
+    print(seed)
+
+
+def start_simulation_2(seed):
     os.system('./waf --run \'dash-btree-botup --stopTime=%d --Client=%d --seed=%d --AdaptationLogicToUse=\'%s\'\'' %
               (sim_params['stopTime'], sim_params['users'], seed, sim_params['protocol']))
+    print(seed)
 
 
 def main():
-    sim_params = {
-        'cores': int(os.getenv('cores')),
-        'users': int(os.getenv('Users')),  # 15
-        'rep': int(os.getenv('Repetition')),
-        'stopTime': int(os.getenv('StopTime')),
-        'protocol': os.getenv('AdaptationLogicToUse'),  # RateBasedAdaptationLogic RateAndBufferBasedAdaptationLogic
-    }
-
     print("Starting users:", sim_params['users'])
 
     q = [i for i in range(sim_params['rep'])]
 
-    jobs = []
-    while q:
-        seed = q.pop()
+    with Pool(5) as p:
+        p.map(start_simulation_2, q)
 
-        job = Process(target=start_simulation, args=(sim_params, seed))
-        jobs.append(job)
-        job.start()
-
-        if len(jobs) >= sim_params['cores'] or not q:
-            while jobs:
-                job = jobs.pop()
-                job.join()
+    # jobs = []
+    # while q:
+    #     seed = q.pop()
+    #
+    #     job = Process(target=start_simulation, args=(sim_params, seed))
+    #     jobs.append(job)
+    #     job.start()
+    #
+    #     if len(jobs) >= sim_params['cores'] or not q:
+    #         while jobs:
+    #             job = jobs.pop()
+    #             job.join()
 
     print("Done.")
 
